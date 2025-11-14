a664530bf0f84b4c202ae6233ccfcd117627efd8
Alana <alanabeatriztobias@gmail.com>
2025-11-14 13:01:11 -0300

    rota criar produto ✔

--- back-end/main.py
+++ back-end/main.py
@@ -7,5 +7,9 @@ app = FastAPI(title="Gerenciador de Produtos")
 def home():
     return {"mensagem": "Bem-vindo ao Gerenciador de Produtos"}
 
-# @app.post("/produtos")
-# def cadastrar_produto(nome: str, categoria: str, preco: float )
+@app.post("/produtos")
+def criar_produto(nome: str, categoria: str, preco: float, quantidade: int):
+    funcao.cadastrar_produto(nome, categoria, preco, quantidade)
+    return {"mensagem": "Produto cadastrado com sucesso!"}
+
+
