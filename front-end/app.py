60326e94ac6414dbc211838a194d7d3101d1b7d2
Alana <alanabeatriztobias@gmail.com>
2025-11-14 15:57:13 -0300

    front-end: função cadastrar produtos ✔

--- front-end/app.py
+++ front-end/app.py
@@ -27,6 +27,27 @@ if menu == "Listar Produtos":
         st.error("Erro ao conectar com a API.")
 
 
+elif menu == "Cadastrar Produtos":
+    st.subheader("📝 Cadastrar novo produto")
+    nome = st.text_input("Nome")
+    categoria = st.text_input("Categoria")
+    preco = st.number_input("Preço", min_value=0.0, format="%.2f")
+    quantidade = st.number_input("Quantidade", min_value=0, step=1)
+    if st.button("Cadastrar"):
+        if nome and categoria:
+            params = {
+                "nome": nome,
+                "categoria": categoria,
+                "preco": preco,
+                "quantidade": quantidade,
+            }
+            response = requests.post(f"{API_URL}/produtos", params=params)
+            if response.status_code == 200:
+                st.success("Produto cadastrado com sucesso!")
+            else:
+                st.error("Erro ao cadastrar produto")
+        else:
+            st.warning("Preencha todos os campos obrigatórios.")
 
 
 
